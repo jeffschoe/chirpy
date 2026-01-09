d44515c5386893bdddcc13d11b7f9dcb0b77101a
jeffschoe <ratacame@protonmail.com>
2026-01-08 22:11:41 -0500

    CH1: Servers, L12 Custom Handlers.

---
+++ src/api/readiness.ts
@@ -0,0 +1,45 @@
+import type { Request, Response } from "express";
+
+
+export async function handlerReadiness(req: Request, res: Response) {
+  /**
+   * This function is responsible for:
+   * Setting the appropriate headers
+   * Sending back the response body.
+   * Express automatically creates the req and res objects for us, 
+   * they are not explicity passed.
+   * The req object will contain details specific to that request 
+   * (like the client's IP address, headers, body, etc.), 
+   * and the res object is what you'll use to craft the response 
+   * for that specific request. It starts off as empty, for us to fill up.
+   */
+
+  logRequest(req);
+  logResponse(res);
+
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send("OK");
+
+  logResponse(res);
+
+}
+
+function logRequest(req: Request) {
+  console.log('==========Request Log==========')
+  console.log(`* Request Method:  ${req.method}`);
+  console.log(`* Request URL:     ${req.url}`);
+  console.log(`* Request Headers: ${JSON.stringify(req.headers)}`);
+  console.log(`* Request Params:  ${JSON.stringify(req.params)}`);
+  console.log(`* Request Query:   ${JSON.stringify(req.query)}`);
+  console.log(`* Request Body:    ${req.body}`);
+  console.log(`* Request IP:      ${req.ip}`);
+}
+
+function logResponse(res: Response) {
+  console.log('==========Response Log==========')
+  console.log(`* Response Headers Sent:    ${JSON.stringify(res.headersSent)}`)
+  console.log(`* Response Locals:          ${JSON.stringify(res.locals)}`)
+  console.log(`* Response Send Date:       ${JSON.stringify(res.sendDate)}`)
+  console.log(`* Response Status Code:     ${JSON.stringify(res.statusCode)}`)
+  console.log(`* Response Status Message:  ${JSON.stringify(res.statusMessage)}`)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
 import express from "express";
 
+import { handlerReadiness } from "./api/readiness.js";
+
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static(`./src"/app"`));
+//app.use - allows you to mount middleware functions
+//middleware are a series of functions that Express calls in order, like a chain, 
+//when an HTTP request comes into your server
+//we are using the express.static middleware, which serves static files
+//arg to express.statis is is the actual file system directory where those files live
+//1st arg, `path`, is the path for which we will call the middleware function/s or,
+//"middleware should only be executed when the URL path of the incoming request 
+//starts with PATH"
+
+app.get('/healthz', handlerReadiness)
+/**
+ * .get: - tells Express to listen for HTTP GET requests specifically
+ * /healthz: This is the path (or route) that the handler will respond to. 
+ * So when someone makes a GET request to http://localhost:8080/healthz, 
+ * this handler will be triggered
+ * handlerReadiness: This is the handler function that will execute when a 
+ * GET request comes in to /healthz
+ * Express automatically creates the req and res objects for us, 
+ * they are not explicity passed
+ */
 
 app.listen(PORT, () => {
   console.log(`Server is runing at http://localhost:${PORT}`)
