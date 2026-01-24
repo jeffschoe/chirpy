8d88b0d9e72fb31468763feb390b8c102dca8c34
jeffschoe <ratacame@protonmail.com>
2026-01-23 21:58:26 -0500

    error.ts comments

--- src/api/errors.ts
+++ src/api/errors.ts
@@ -1,22 +1,22 @@
-export class BadRequestError extends Error {
+export class BadRequestError extends Error { //400
   constructor(message: string) {
     super(message);
   }
 }
 
-export class UserNotAuthenticatedError extends Error {
+export class UserNotAuthenticatedError extends Error { //401
   constructor(message: string) {
     super(message);
   }
 }
 
-export class UserForbiddenError extends Error {
+export class UserForbiddenError extends Error { //403
   constructor(message: string) {
     super(message);
   }
 }
 
-export class NotFoundError extends Error {
+export class NotFoundError extends Error { //404
   constructor(message: string) {
     super(message);
   }
